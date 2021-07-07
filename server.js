75c35da854272b6465ea0388d6d5fd7dafc52a55
xorua117 <xorua117@gmail.com>
2021-07-07 20:40:20 +0900

    Sending active rooms to all clients - server

--- server.js
+++ server.js
@@ -52,6 +52,11 @@ io.on('connection', (socket) => {
       event: broadcastEventTypes.ACTIVE_USERS,
       activeUsers: peers
     });
+
+    io.sockets.emit('broadcast', {
+      event: broadcastEventTypes.GROUP_CALL_ROOMS,
+      groupCallRooms
+    });
   });
 
   socket.on('disconnect', () => {
@@ -119,6 +124,9 @@ io.on('connection', (socket) => {
     };
 
     groupCallRooms.push(newGroupCallRoom);
-    console.log(groupCallRooms);
+    io.sockets.emit('broadcast', {
+      event: broadcastEventTypes.GROUP_CALL_ROOMS,
+      groupCallRooms
+    });
   });
 });
